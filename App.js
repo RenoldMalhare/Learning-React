a623bb465f4476ddc6713fa281df5e3641602e1a
RenoldMalhare <renoldmalhare2@gmail.com>
2025-06-27 15:35:27 +0530

    basics of React

---
+++ App.js
@@ -0,0 +1,41 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World form React."
+// );
+// console.log(heading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+//<div id="parent">
+//   <div id="child">
+//      <h1></h1>
+//      <h2></h2>
+//   </div>
+//   <div id="child">
+//      <h1></h1>
+//      <h2></h2>
+//   </div>
+// </div>
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "I am h1 tag."),
+      React.createElement("h2", {}, "I am h2 tag."),
+    ]),
+  ],
+  [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "I am h1 tag."),
+      React.createElement("h2", {}, "I am h2 tag."),
+    ]),
+  ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+# React
+
+- Library , Framework, Emmet, Crossorigin
+- Creating Basic React app Direct By Html and javascript.

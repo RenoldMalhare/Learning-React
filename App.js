792a5b7b48464e6af508516b5b0f9ab97b17a136
RenoldMalhare <renoldmalhare2@gmail.com>
2025-07-03 20:00:03 +0530

    React Basics

--- App.js
+++ App.js
@@ -1,31 +1,34 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World form React."
-// );
-// console.log(heading);
-// const root = ReactDOM.createRoot(document.getElementById("root"));
+// import React from "react";
+// import ReactDOM from "react-dom/client";
 
-// root.render(heading);
+// // const heading = React.createElement(
+// //   "h1",
+// //   { id: "heading" },
+// //   "Hello World form React."
+// // );
+// // console.log(heading);
+// // const root = ReactDOM.createRoot(document.getElementById("root"));
 
-//<div id="parent">
-//   <div id="child">
-//      <h1></h1>
-//      <h2></h2>
-//   </div>
-//   <div id="child">
-//      <h1></h1>
-//      <h2></h2>
-//   </div>
-// </div>
+// // root.render(heading);
+
+// //<div id="parent">
+// //   <div id="child">
+// //      <h1></h1>
+// //      <h2></h2>
+// //   </div>
+// //   <div id="child">
+// //      <h1></h1>
+// //      <h2></h2>
+// //   </div>
+// // </div>
 
 const parent = React.createElement(
   "div",
   { id: "parent" },
   [
     React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I am h1 tag."),
-      React.createElement("h2", {}, "I am h2 tag."),
+      React.createElement("h1", {}, "This is React Basics."),
+      React.createElement("h2", {}, "Learning renold."),
     ]),
   ],
   [
--- ReadMe.md
+++ ReadMe.md
@@ -1,4 +1,16 @@
 # React
 
-- Library , Framework, Emmet, Crossorigin
+\*\* Importent thing to remember.
+
+- CDN -A Content Delivery Network (CDN) is a geographically distributed network of servers and data centers that work together to deliver web content and media to users more quickly and reliably by caching copies of content closer to the users' locations. This reduces the distance data must travel, improving website loading speed, reducing latency, and enhancing user experience.
+
+- Crossorigin -crossorigin is an HTML attribute used on elements like <script>, <img>, and <link> to control how browsers handle cross-origin requests for these resources. It is closely related to the concept of Cross-Origin Resource Sharing (CORS), which is a security mechanism that allows servers to specify who can access their resources from different origins
+- Caching is the process of storing frequently accessed data or resources in a temporary storage location called a cache, so that future requests for that data can be served much faster. The main purpose of caching is to improve system performance and reduce latency by avoiding repeated access to slower storage or data sources
+
+- Library , Framework, Emmet,
 - Creating Basic React app Direct By Html and javascript.
+
+# Parcel
+
+- HMR (Hot Module Render)
+- Zero config
